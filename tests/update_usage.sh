4e5db983a202f5372311ad8dfde9592e969944a0
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-19 16:43:37 +0100

    Delete output that isn't useful to users.

--- tests/update_usage.sh
+++ tests/update_usage.sh
@@ -10,12 +10,12 @@ set -o pipefail
 
 mkdir -p downloads-for-docs-ungrouped
 pushd    downloads-for-docs-ungrouped
-../../obsidian-repos-downloader.py  --limit 4 2>&1 > ../console-output-ungrouped.txt
-tree -L 1 -d plugins themes                   2>&1 > ../tree-output-ungrouped.txt
+../../obsidian-repos-downloader.py  --limit 4
+tree -L 1 -d plugins themes   2>&1 > ../tree-output-ungrouped.txt
 popd
 
 mkdir -p downloads-for-docs-grouped
 pushd    downloads-for-docs-grouped
-../../obsidian-repos-downloader.py  --limit 4 --group-by-user     2>&1 > ../console-output-grouped.txt
-tree -L 2 -d plugins themes                                       2>&1 > ../tree-output-grouped.txt
+../../obsidian-repos-downloader.py  --limit 4 --group-by-user
+tree -L 2 -d plugins themes   2>&1 > ../tree-output-grouped.txt
 popd
