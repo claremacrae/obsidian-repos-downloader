b932e93388f6d0071c534306f92ae03191ea28c1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-10-21 14:51:42 +0100

    Update the tree of output files for addition of snippets

--- tests/update_usage.sh
+++ tests/update_usage.sh
@@ -11,11 +11,11 @@ set -o pipefail
 mkdir -p downloads-for-docs-ungrouped
 pushd    downloads-for-docs-ungrouped
 ../../obsidian-repos-downloader.py  --limit 4
-tree -L 1 -d plugins themes   2>&1 > ../tree-output-ungrouped.txt
+tree -L 1 -d plugins snippets themes   2>&1 > ../tree-output-ungrouped.txt
 popd
 
 mkdir -p downloads-for-docs-grouped
 pushd    downloads-for-docs-grouped
 ../../obsidian-repos-downloader.py  --limit 4 --group-by-user
-tree -L 2 -d plugins themes   2>&1 > ../tree-output-grouped.txt
+tree -L 2 -d plugins snippets themes   2>&1 > ../tree-output-grouped.txt
 popd
